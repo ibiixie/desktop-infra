d08cb32e3f43b126e5e2a857b434d948e6fa8781
biixie <65206220+ibiixie@users.noreply.github.com>
2025-09-03 17:22:39 +0200

    fix(ci): setup ssh properly

--- tunnel_setup.sh
+++ tunnel_setup.sh
@@ -10,6 +10,13 @@ echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudf
 # Install cloudflared
 sudo apt-get update && sudo apt-get install cloudflared
 
+# Setup SSH dir and config perms
+echo Setting up SSH directory and config
+mkdir -p /root/.ssh
+touch /root/.ssh/config
+chmod 600 /root/.ssh/config
+chmod 700 /root/.ssh
+
 # Add SSH ProxyCommand for cloudflared
 echo Added ProxyCommand to config at $SSH_CONFIG_FILE
 echo "Host $REMOTE_HOST" >> $SSH_CONFIG_FILE
