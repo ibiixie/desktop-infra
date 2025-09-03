c8d65c8aeaaf82dc1a10416de2dbff2d18fe4ee2
biixie <65206220+ibiixie@users.noreply.github.com>
2025-09-03 17:08:11 +0200

    fix(ci): install cloudflared via cloudflare package repository
    Doesn't exist in the Ubuntu repository by default.

--- tunnel_setup.sh
+++ tunnel_setup.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-apt-get update && apt-get install cloudflared
+# Add cloudflare gpg key
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
 
+# Add this repo to your apt repositories
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared noble main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+# Install cloudflared
+sudo apt-get update && sudo apt-get install cloudflared
+
+# Add SSH ProxyCommand for cloudflared
 echo "Host $REMOTE_HOST" >> $SSH_CONFIG_FILE
 echo "    ProxyCommand cloudflared access ssh --hostname %h --id $CF_TUNNEL_ID --token $CF_TUNNEL_SECRET" >> $SSH_CONFIG_FILE
 
